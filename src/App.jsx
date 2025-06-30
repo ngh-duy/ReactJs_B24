f63582d1b4794e7695d4db660994e11a8f0f6c50
21DTHA2_2180605768 <duynguyenhoang203@gmail.com>
2025-06-30 08:27:28 +0700

    Thay doi noi dung

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import BT1 from "./bt1";
-import Rendering from "./rendering-element/index"
-import Event from "./event-click/index"
-import State from "./state/index"
-import ChangeColorCard from "./changesColorCar";
-import ListKey from "./listKey/index";
+// import Rendering from "./rendering-element/index"
+// import Event from "./event-click/index"
+// import State from "./state/index"
+// import ChangeColorCard from "./changesColorCar";
+// import ListKey from "./listKey/index";
 import ChangeGlasses from "./changesGlasses";
 function App() {
 
